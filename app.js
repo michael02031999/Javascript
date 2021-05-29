d576df3891b13b32feea31cd700d85cd5e7ab7d1
Michael <michael.galan@uconn.edu>
2021-05-29 10:14:20 -0400

    ToDo List

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-alert("I am a big asshole");
-
-
 let array = [];
 let string = "";
 do {
@@ -8,6 +5,24 @@ do {
     if (string === "new") {
         let toDo = prompt("Add your toDo");
         array.push(toDo);
-        console.log(array);
+        console.log(`${toDo} added to the list`);
+        
+    }
+    if (string === "list") {
+        console.log("********");  
+        for (let i = 0; i < array.length; i++) {
+            console.log(i + ": " + array[i]);
+        }
+        console.log("********");    
+    }
+
+    if (string === "delete"){
+        index = Number(prompt("Enter index of todo to delete"))
+        array.splice(index, 1);
+        console.log("ToDo Removed");
+    }
+
+    if (string === "quit") {
+        console.log("OK, YOU QUIT THE APP");
     }
 } while ((string !== "quit"));
