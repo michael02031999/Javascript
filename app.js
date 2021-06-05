d6c75a38bb72f44d119a03126c5331988b96d4e3
Michael <michael.galan@uconn.edu>
2021-06-05 13:51:19 -0400

    Update

--- app.js
+++ app.js
@@ -1,59 +1,24 @@
-let array = [];
-let string = "";
-do {
-    string = prompt("What would you like to do?");
-    if (string === "new") {
-        let toDo = prompt("Add your toDo");
-        array.push(toDo);
-        console.log(`${toDo} added to the list`);
-        
-    }
-    if (string === "list") {
-        console.log("********");  
-        for (let i = 0; i < array.length; i++) {
-            console.log(i + ": " + array[i]);
-        }
-        console.log("********");    
-    }
-
-    if (string === "delete"){
-        index = Number(prompt("Enter index of todo to delete"))
-        array.splice(index, 1);
-        console.log("ToDo Removed");
-    }
-
-    if (string === "quit") {
-        console.log("OK, YOU QUIT THE APP");
-    }
-} while ((string !== "quit"));
-
-function range(a,b, step) {
-    if (step === undefined) {
-        step=1;
-    }
-    let array=[];
-    if(a<b) {
-        for (let i = a; i <= b; i=i+step) {
-            array.push(i);
+function countBs(string) {
+    let count=0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i]==='B') {
+            count=count+1;
         }
     }
-    if (a > b) {
-        for (let i = a; i>=b; i=i+step) {
-            array.push(i);
-        }
-    }
-    return array;
+    return count;
 }
 
-function sum(array) {
-    let total = 0;
-    for(let i = 0; i < array.length; i++) {
-        total = total + array[i];
+console.log(countBs('BasBak'));
+
+function countChar(string, index, ) {
+    let countTwo=0
+    for (let i = 0; i < string.length; i++) {
+        if (string[i]===index) {
+            countTwo+=1;
+        }
     }
-    return total;
+    return countTwo;
 }
 
 
-
-
-
+console.log(countChar("kakkerlak", "k"));
