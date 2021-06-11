eb14504ad0e96049510c90252128829763899495
Michael <michael.galan@uconn.edu>
2021-06-11 18:08:28 -0400

    This is torture

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-
-    function arrayToList(array) {
-        let result = {};
-        if (Array.isArray(array)) {
-          let currListItem = result;
-          for (let item of array) {
-           
-            let newListItem = {
-                value: item,
-                rest: null
-            };
-            console.log({}.rest);
-            if (typeof currListItem.rest === 'undefined') {
-                
-              result = newListItem;
-            } else {
-                
-                currListItem.rest = newListItem;
-            }
-            currListItem = newListItem;
-          }
-        }
+function arrayToList(array) {
+    let result = {};
+    if (Array.isArray(array)) {
+      let currListItem = result;
+      for (let item of array) {
+        console.log(currListItem);
         
+        let newListItem = {
+            value: item,
+            rest: null
+        };
 
-        //return result;
+        console.log(newListItem);
 
-      }
+        if (typeof currListItem.rest === 'undefined') {
+          result = newListItem;
+        } 
+        else {
+            currListItem.rest = newListItem;
+        }
+        currListItem = newListItem;
+        console.log(currListItem);
+    }
+    //console.log(result)
+    }
+  }
 
+  arrayToList([10,20,30]);
 
-
-
-arrayToList([10,20,30]);
