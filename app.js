6d920ade5736968691138acf65841f6c4dce6b58
Michael <michael.galan@uconn.edu>
2021-06-22 13:05:53 -0400

    More Javascript

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
-function arrayToList(array) {
-    let list = null; 
-    for (let i = array.length; i >= 0; i--) {
-        list = {value: array[i], rest: list};
-    }
-    return list; 
-}
+let cities = [
+    {name: 'Los Angeles', population: 3702621},
+    {name: 'New York', population: 8175133},
+    {name: 'Chicago', population: 2695598},
+    {name: 'Houston', population: 2099451},
+    {name: 'Philadelphia', population: 1526006}
+]
 
-function listToArray (list) {
-    let array = [];
-    for (let node = list; node; node=node.rest){
-        if (node.value !== undefined) {   
-            array.push(node.value);
-        } 
-    }
-    return array;
-}
+let newCities = cities.filter(({population}) => {
+    return population
+})
 
-function prepend(element, list) {
-    let newList={};
-    newList.value=element;
-    newList.rest=list;
-    return newList;
-}
-
-function nth (list, number) {
-    let array =[];
-    for (let node = list; node; node = node.rest) {
-        if(node.value!==undefined) {
-            array.push(node.value);
-        }  
-    }
-    return array[number];
-}
-
-
-console.log(nth(arrayToList([10,20,30]), 0))
-
-//console.log(prepend(10, prepend(20, null)));
-
-//console.log(arrayToList([10,20,30]));
-//console.log(listToArray(arrayToList([10,20,30])));
+console.log(newCities)
