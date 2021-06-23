b8482dd8221b9b033e686a5da8ea3579c900280d
Michael <michael.galan@uconn.edu>
2021-06-23 13:48:58 -0400

    Chapter 5 of Eloquent Javascript

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-let cities = [
-    {name: 'Los Angeles', population: 3702621},
-    {name: 'New York', population: 8175133},
-    {name: 'Chicago', population: 2695598},
-    {name: 'Houston', population: 2099451},
-    {name: 'Philadelphia', population: 1526006}
-]
+let arrays = [[1,2,3], [4,5], [6]];
 
-let newCities = cities.filter(({population}) => {
-    return population
+let newArray = arrays.reduce(function (array1, array2) {
+    return array1.concat(array2);
 })
 
-console.log(newCities)
+//console.log(newArray);
+
+function loop (value, testFunc, updateFunc, bodyFunc) {
+    do {
+        bodyFunc(value);
+        value = updateFunc(value);
+    } while (testFunc(value));
+}
+
+//loop(5, n => n > 0, n => n - 1, console.log);
+
+function every(array, test) {
+    let newArr=[];
+    for (let i = 0; i < array.length; i++) {
+        if (test(array[i]) == false) {
+            return false;
+        } 
+    }
+    return true;
+}
+
+//console.log(every([1,3,5], n => n <3));
+
+function every2(array, test) {
+    return !array.some(element => 
+        console.log(!test(element)));
+}
+
+console.log(every2([1, 3, 5], n => n < 10));
