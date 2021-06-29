8da48ae3e6b88c421c15cf3649bc299a882fb1ce
Michael <michael.galan@uconn.edu>
2021-06-29 15:25:20 -0400

    Still learning Javascript

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-let arrays = [[1,2,3], [4,5], [6]];
+class Vec {
 
-let newArray = arrays.reduce(function (array1, array2) {
-    return array1.concat(array2);
-})
-
-//console.log(newArray);
-
-function loop (value, testFunc, updateFunc, bodyFunc) {
-    do {
-        bodyFunc(value);
-        value = updateFunc(value);
-    } while (testFunc(value));
-}
-
-//loop(5, n => n > 0, n => n - 1, console.log);
-
-function every(array, test) {
-    let newArr=[];
-    for (let i = 0; i < array.length; i++) {
-        if (test(array[i]) == false) {
-            return false;
-        } 
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;    
     }
-    return true;
+
+    plus(object) {
+        return (new Vec(this.x + object.x, this.y + object.y));
+    }
+
+    minus(object) {
+        return (new Vec(this.x - object.x,this.y - object.y));
+    }
+
+    get length() {
+        console.log(Math.sqrt(this.x*this.x + this.y*this.y));
+    }
+    
 }
 
-//console.log(every([1,3,5], n => n <3));
+console.log(new Vec(1, 2));
 
-function every2(array, test) {
-    return !array.some(element => 
-        console.log(!test(element)));
-}
 
-console.log(every2([1, 3, 5], n => n < 10));
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+new Vec(3, 4).length;
